6d8a4d2345e9d892452b5caad2ad92ded72ea0ee
Gshanley <gavins22.gs@gmail.com>
2020-08-03 08:54:07 -0400

    Gavin's Movement
    Broken attempts that still need work

---
+++ Assets/MousePoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MousePoint : MonoBehaviour
+{
+    public GameObject temp;
+    public GameObject floor;
+
+    void Update()
+    {
+        //Vector3 mouse = Mouse.current.position;
+        Vector2 Mousepoint = Camera.main.ScreenToWorldPoint(Mouse.current.position);
+        Debug.Log(Mousepoint);
+        Debug.Log(Input.mousePosition);
+        Vector3 dir = Mousepoint - Camera.main.transform.position;
+
+        RaycastHit hit;
+        Physics.Raycast(Mousepoint, dir, out hit);
+        temp.transform.position = hit.point;
+    }
+}
